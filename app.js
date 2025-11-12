42957dad80ed6ead2f3167efbe614048f0bf875f
renacod <renacod000@gmail.com>
2025-11-12 21:23:39 +0530

    changed the format of cicd

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-// Import the express module
 const express = require('express');
 
-// Create an Express app
 const app = express();
 
-// Define a port
 const PORT = 3000;
 
-// Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, World! Hi HariPrasath!');
+  res.send('Hello, World! Hi HariPrasath');
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
